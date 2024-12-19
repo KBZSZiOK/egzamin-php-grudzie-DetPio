1c3c788d211aebabb0ea9c392cd680b4a6d3dee7
DetPio <156914223+DetPio@users.noreply.github.com>
2024-12-19 14:38:53 +0100

    Add files via upload

--- egzamin/rezerwacja.php
+++ egzamin/rezerwacja.php
@@ -1,5 +1,22 @@
 <?php
 
+$conn = mysqli_connect("localhost", "root", "", "baza2");
 
+if(!$conn){
+    die("Brak połączenia z bazą danych: ". mysqli_connect_error());
+}
 
+$data = $_POST['data'];
+$osoby = $_POST['osoby'];
+$numer = $_POST['numer'];
+
+$sql = "INSERT INTO rezerwacje (data_rez, liczba_osob, telefon) VALUES ('$data', '$osoby', '$numer')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Dodano rezerwację do bazy";
+} else {
+    echo "Błąd: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
